ef64ab1f0c3757a8eb4333f381675e809fd6a764
Christianib003 <christianib003@gmail.com>
2022-11-23 15:42:24 +0200

    migrate posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_133750) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_134106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_133750) do
     t.integer "authorId"
     t.integer "postId"
     t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "authorId"
+    t.string "title"
+    t.string "text"
+    t.integer "commentsCounter"
+    t.integer "likesCounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
