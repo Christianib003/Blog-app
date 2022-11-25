f854724c7b4a17d76989dcebf976bbf434f97a5c
Christianib003 <christianib003@gmail.com>
2022-11-25 19:27:03 +0200

    add tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, foreign_key: :post_id
 
-  validates :title, presence: true, length:{maximum: 250}
-  validates :comments_counter, numerically:{only_integer: true, greater_than_or_equal_to: 0}
-  validates :likes_counter, numerically:{only_integer: true, greater_than_or_equal_to: 0}
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
     author.increment!(:posts_counter)
