e5beae2611e57144b764b7a4d5a01c6a48ef632d
Christianib003 <christianib003@gmail.com>
2022-11-29 13:02:17 +0200

    add tests for users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,22 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
       get '/users'
-      expect(response).to have_http_status(:ok)
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'displays the intended template' do
+      get '/users'
+      expect(response).to render_template('index')
+    end
+
+    it 'returns the http sucess' do
+      get '/users/:id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'displays the intended template' do
+      get '/users/:id'
+      expect(response).to render_template('show')
     end
   end
 end
