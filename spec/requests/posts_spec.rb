75dcd14bcc01bd35613aa47d9bca72ec7405e118
Christianib003 <christianib003@gmail.com>
2022-11-29 12:58:55 +0200

    add additional tests for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,5 +11,16 @@ RSpec.describe 'Posts', type: :request do
       get '/users/:user_id/posts'
       expect(response).to render_template('index')
     end
+
+    it 'returns the http sucess' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'displays the intended template' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to render_template('show')
+    end
+
   end
 end
