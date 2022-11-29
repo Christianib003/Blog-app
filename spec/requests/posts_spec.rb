cfcb14c64024219bc05116f3f48a9e35e0444d9b
Christianib003 <christianib003@gmail.com>
2022-11-29 12:53:01 +0200

    add tests for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get '/users/:user_id/posts'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'displays the intended template' do
+      get '/users/:user_id/posts'
+      expect(response).to render_template('index')
     end
   end
 end
