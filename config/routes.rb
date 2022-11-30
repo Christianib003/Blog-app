df68a424b37a8679f811b758a783be19c8c29b45
Christianib003 <christianib003@gmail.com>
2022-11-30 15:35:03 +0200

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  root "users#index"
-
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:id", to: "posts#show"
+  get 'posts/index'
+  get 'users/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root 'users#index'
+  # Defines the route for the users index page ("/users")
+  resources :users, only: [:index, :show] do
+    # Defines the route for the posts index page ("/users/:user_id/posts")
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:create, :new]
+    end
+  end
 end
