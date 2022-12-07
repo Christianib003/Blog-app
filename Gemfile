915adf7612da3eabb4576b47a675bba2288fda77
PRATAP-KUMAR <pratap@fastmail.fm>
2022-12-07 19:33:04 +0530

    Wrote integration tests for user-index and user-show pages, fix linters

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.1.2'
+ruby '3.0.4'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.4'
@@ -50,12 +50,12 @@ gem 'bootsnap', require: false
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem 'debug', platforms: %i[mri mingw x64_mingw]
+  # gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails'
-  gem "database_cleaner"
-  gem 'webdrivers'
+  # gem "database_cleaner"
   gem 'capybara'
-  gem 'capybara-screenshot'
+  gem 'webdrivers'
+  # gem 'capybara-screenshot'
 end
 
 group :development do
@@ -71,12 +71,12 @@ end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
-  gem 'capybara'
   gem 'rails-controller-testing'
   gem 'selenium-webdriver'
-  gem 'webdrivers'
 end
 
 gem 'ffi'
 
 gem 'bullet', group: 'development'
+
+gem 'digest'
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,6 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "UserShows", type: :system do
+RSpec.describe 'UserShows', type: :system do
   subject { User.new(name: 'Christian', posts_counter: 0, photo: 'https://randomuser.me/api/portraits/men/70.jpg', bio: 'Student at Microverse') }
 
   before { subject.save }
@@ -9,6 +9,75 @@ RSpec.describe "UserShows", type: :system do
     it 'I can see the username of all other users.' do
       visit root_path(subject)
       page.has_content?(subject.name)
+    end
+
+    it 'profile picture for each user.' do
+      visit root_path(subject)
+      page.has_css?('.img')
+    end
+
+    it 'I can see the number of posts each user has written.' do
+      visit root_path(subject)
+      page.has_content?(subject.posts_counter)
+    end
+
+    it "When I click on a user, I am redirected to that user's show page." do
+      User.delete_all
+      user = User.create(name: 'Christian', posts_counter: 0, photo: 'https://randomuser.me/api/portraits/men/70.jpg',
+                         bio: 'Student at Microverse.')
+      visit root_path(user)
+      click_on 'Christian'
+      visit user_path(user.id)
+      page.has_content?('Christian')
+    end
+  end
+
+  describe 'user show page' do
+    it "I can see the user's profile picture." do
+      visit user_path(subject.id)
+      page.has_css?('.img')
+    end
+
+    it "I can see the user's username." do
+      visit user_path(subject.id)
+      page.has_content?(subject.name)
+    end
+
+    it 'I can see the number of posts the user has written.' do
+      visit user_path(subject.id)
+      page.has_content?(subject.posts_counter)
+    end
+    it "I can see the user's bio." do
+      visit user_path(subject.id)
+      page.has_content?(subject.bio)
+    end
+    it "I can see the user's first 3 posts." do
+      Post.create([{ author: subject, title: 'First Post', text: 'My first post' },
+                   { author: subject, title: 'Second Post', text: 'My Second post' },
+                   { author: subject, title: 'Third Post', text: 'My Third post' }])
+      visit user_path(subject.id)
+      page.has_content?(subject.posts)
+    end
+
+    it "I can see a button that lets me view all of a user's posts." do
+      visit user_path(subject.id)
+      page.has_button?('See All Posts')
+    end
+
+    it "When I click a user's post, it redirects me to that post's show page." do
+      post = Post.create(author: subject, title: 'First Post', text: 'My first post')
+      visit user_path(subject.id)
+      click_on 'First Post'
+      visit user_post_path(subject.id, post.id)
+      page.has_content?(post.title)
+    end
+
+    it "When I click to see all posts, it redirects me to the user's post's index page." do
+      visit user_path(subject.id)
+      sleep(5)
+      click_on 'See All Posts'
+      visit user_posts_path(subject.id)
+      page.has_content?('Christian')
     end
   end
 end
